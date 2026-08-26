9cb9bb240d92b5a2fac9f80be425b6e3775849b5
Emilia Masiak <90275335+Emka5885@users.noreply.github.com>
2026-08-26 14:54:38 +0200

    Test for main validation logic - data types

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from timeseries_validator.validator import validate
 from timeseries_validator.contract import ValidationContract, ColumnRules
-from timeseries_validator.issues import ValidationCode
+from timeseries_validator.issues import ValidationCode, Severity
 
 def test_validate_empty_dataset():
     test_df = pd.DataFrame()
@@ -15,9 +15,9 @@ def test_validate_empty_dataset():
     assert issues[0].code == ValidationCode.EMPTY_DATASET
 
 def test_validate_required_columns():
-    test_df = pd.DataFrame({"test": [0]})
+    test_df = pd.DataFrame({"test1": [0]})
     validation_contract = ValidationContract(require_non_empty=True, columns={
-        "test" : ColumnRules(required=True),
+        "test1" : ColumnRules(required=True),
         "test2": ColumnRules(required=True)
     })
 
@@ -30,9 +30,9 @@ def test_validate_required_columns():
     assert "test2" in issues[0].message
 
 def test_validate_missing_values():
-    test_df = pd.DataFrame({"test": [None, 1], "test2": [2, None]})
+    test_df = pd.DataFrame({"test1": [None, 1], "test2": [2, None]})
     validation_contract = ValidationContract(require_non_empty=False, columns={
-        "test" : ColumnRules(no_missing_values=False),
+        "test1" : ColumnRules(no_missing_values=False),
         "test2": ColumnRules(no_missing_values=True)
     })
 
@@ -43,4 +43,47 @@ def test_validate_missing_values():
     assert len(issues) == 1
     assert issues[0].code == ValidationCode.MISSING_VALUES
     assert "test2" in issues[0].message
+
+def test_validate_data_types():
+    test_df = pd.DataFrame({"test1": [0.1, 1.5, None], "test2": [2.1, 3.7, "abc"], "test3": [0.1, 9, True]})
+    validation_contract = ValidationContract(require_non_empty=True, columns={
+        "test1" : ColumnRules(data_type=float),
+        "test2": ColumnRules(data_type=float),
+        "test3": ColumnRules(data_type=float)
+    })
+
+    # Validate the DataFrame with wrong data types
+    issues = validate(test_df, validation_contract)
+
+    # Should skip missing values
+    # Check that the correct issues are returned
+    assert len(issues) == 3
+
+    # "abc" cannot be converted from str to float - so error
+    assert any(
+        issue.code == ValidationCode.WRONG_DATA_TYPE
+        and "test2" in issue.message
+        and "for rows: [2]" in issue.message
+        and issue.severity == Severity.ERROR
+        for issue in issues
+    )
+
+    # 9 can be converted from int to float - so warning
+    assert any(
+        issue.code == ValidationCode.WRONG_DATA_TYPE
+        and "test3" in issue.message
+        and "for rows: [1]" in issue.message
+        and issue.severity == Severity.WARNING
+        for issue in issues
+    )
+
+    # True cannot be converted from bool to float - so error
+    assert any(
+        issue.code == ValidationCode.WRONG_DATA_TYPE
+        and "test3" in issue.message
+        and "for rows: [2]" in issue.message
+        and issue.severity == Severity.ERROR
+        for issue in issues
+    )
+
 
